4a2928dbaf9bfc772df4f15f92f3940862141c38
josephcox9621 <josephcox2@btinternet.com>
2025-02-09 13:30:12 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('daily-tracker-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
 
